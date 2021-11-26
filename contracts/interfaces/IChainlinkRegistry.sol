d48286ccb96aad5ad15e939861be90faa4be0b1b
Nicolas Chamo <nicolas@chamo.com.ar>
2021-11-26 09:33:44 -0300

    feat: adding registry (#1)
    * feat: redirect calls from feed proxy (#2)
    * test: add integration test (#3)
    * feat: add collectable-dust (#4)

---
+++ contracts/interfaces/IChainlinkRegistry.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.7 <0.9.0;
+
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol';
+import './utils/IGovernable.sol';
+import './utils/ICollectableDust.sol';
+
+interface IChainlinkRegistry is IGovernable, ICollectableDust {
+  /// @notice Thrown when one of the parameters is a zero address
+  error ZeroAddress();
+
+  /// @notice Thrown when trying to execute a call with a base and quote that don't have a feed assigned
+  error FeedNotFound();
+
+  /// @notice Emitted when a feed is set
+  /// @param base The base asset address
+  /// @param quote The quote asset address
+  /// @param feed The feed address
+  event FeedSet(address base, address quote, address feed);
+
+  /// @notice Returns the proxy feed for a specific quote and base
+  /// @dev Will revert with `FeedNotFound` if no feed is found for the given base and quote
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @return The feed's address
+  function getFeedProxy(address _base, address _quote) external view returns (AggregatorV2V3Interface);
+
+  /// @notice Returns the number of decimals present in the response value.
+  /// @dev Will revert with `FeedNotFound` if no feed is found for the given base and quote
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @return The number of decimals in the response
+  function decimals(address _base, address _quote) external view returns (uint8);
+
+  /// @notice Returns the description of the underlying aggregator that the proxy points to.
+  /// @dev Will revert with `FeedNotFound` if no feed is found for the given base and quote
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @return The description of the underlying aggregator
+  function description(address _base, address _quote) external view returns (string memory);
+
+  /// @notice Returns the version representing the type of aggregator the proxy points to.
+  /// @dev Will revert with `FeedNotFound` if no feed is found for the given base and quote
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @return The version of the type of aggregator
+  function version(address _base, address _quote) external view returns (uint256);
+
+  /// @notice Returns the version representing the type of aggregator the proxy points to.
+  /// @dev Will revert with `FeedNotFound` if no feed is found for the given base and quote
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @return _roundId The round ID
+  /// @return _answer The price
+  /// @return _startedAt Timestamp of when the round started
+  /// @return _updatedAt Timestamp of when the round was updated
+  /// @return _answeredInRound The round ID of the round in which the answer was computed
+  function latestRoundData(address _base, address _quote)
+    external
+    view
+    returns (
+      uint80 _roundId,
+      int256 _answer,
+      uint256 _startedAt,
+      uint256 _updatedAt,
+      uint80 _answeredInRound
+    );
+
+  /// @notice Sets a proxy feed for a specific quote and base
+  /// @param _base The base asset address
+  /// @param _quote The quote asset address
+  /// @param _feed The feed's address (could be the zero address to delete a feed)
+  function setFeedProxy(
+    address _base,
+    address _quote,
+    address _feed
+  ) external;
+}
---
+++ test/unit/ChainlinkRegistry/chainlink-registry.spec.ts
@@ -0,0 +1,161 @@
+import chai, { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { behaviours, constants } from '@test-utils';
+import { contract, given, then, when } from '@test-utils/bdd';
+import { snapshot } from '@test-utils/evm';
+import { AggregatorV3Interface, ChainlinkRegistry, ChainlinkRegistry__factory, IERC20 } from '@typechained';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
+import { FakeContract, smock } from '@defi-wonderland/smock';
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { BigNumber } from 'ethers';
+
+chai.use(smock.matchers);
+
+contract('ChainlinkRegistry', () => {
+  const LINK = '0xa36085F69e2889c224210F603D836748e7dC0088';
+  const USD = '0x0000000000000000000000000000000000000348';
+
+  let governor: SignerWithAddress;
+  let feed: FakeContract<AggregatorV3Interface>;
+  let registry: ChainlinkRegistry;
+  let token: FakeContract<IERC20>;
+  let snapshotId: string;
+
+  before('Setup accounts and contracts', async () => {
+    [, governor] = await ethers.getSigners();
+    const factory: ChainlinkRegistry__factory = await ethers.getContractFactory(
+      'contracts/ChainlinkRegistry/ChainlinkRegistry.sol:ChainlinkRegistry'
+    );
+    registry = await factory.deploy(governor.address);
+    feed = await smock.fake('AggregatorV3Interface');
+    token = await smock.fake('IERC20');
+    token.transfer.returns(true);
+    snapshotId = await snapshot.take();
+  });
+
+  beforeEach('Deploy and configure', async () => {
+    await snapshot.revert(snapshotId);
+  });
+
+  describe('setFeedProxy', () => {
+    when('zero address is sent for base address', () => {
+      then('reverts with message', async () => {
+        await behaviours.txShouldRevertWithMessage({
+          contract: registry.connect(governor),
+          func: 'setFeedProxy',
+          args: [constants.ZERO_ADDRESS, USD, feed.address],
+          message: 'ZeroAddress',
+        });
+      });
+    });
+    when('zero address is sent for quote address', () => {
+      then('reverts with message', async () => {
+        await behaviours.txShouldRevertWithMessage({
+          contract: registry.connect(governor),
+          func: 'setFeedProxy',
+          args: [LINK, constants.ZERO_ADDRESS, feed.address],
+          message: 'ZeroAddress',
+        });
+      });
+    });
+    when('setting a feed', () => {
+      let tx: TransactionResponse;
+      given(async () => {
+        tx = await registry.connect(governor).setFeedProxy(LINK, USD, feed.address);
+      });
+      then('it is set correctly', async () => {
+        expect(await registry.getFeedProxy(LINK, USD)).to.equal(feed.address);
+      });
+      then('event is emitted', async () => {
+        await expect(tx).to.emit(registry, 'FeedSet').withArgs(LINK, USD, feed.address);
+      });
+    });
+    when('removing a feed', () => {
+      let tx: TransactionResponse;
+      given(async () => {
+        await registry.connect(governor).setFeedProxy(LINK, USD, feed.address);
+        tx = await registry.connect(governor).setFeedProxy(LINK, USD, constants.ZERO_ADDRESS);
+      });
+      then('feed is removed correctly', async () => {
+        await expect(registry.getFeedProxy(LINK, USD)).to.be.revertedWith('FeedNotFound');
+      });
+      then('event is emitted', async () => {
+        await expect(tx).to.emit(registry, 'FeedSet').withArgs(LINK, USD, constants.ZERO_ADDRESS);
+      });
+    });
+    behaviours.shouldBeExecutableOnlyByGovernor({
+      contract: () => registry,
+      funcAndSignature: 'setFeedProxy',
+      params: () => [constants.NOT_ZERO_ADDRESS, constants.NOT_ZERO_ADDRESS, constants.NOT_ZERO_ADDRESS],
+      governor: () => governor,
+    });
+  });
+
+  describe('sendDust', () => {
+    behaviours.shouldBeExecutableOnlyByGovernor({
+      contract: () => registry,
+      funcAndSignature: 'sendDust',
+      params: () => [constants.NOT_ZERO_ADDRESS, token.address, 2000],
+      governor: () => governor,
+    });
+  });
+
+  redirectTest({
+    method: 'decimals',
+    returnsWhenMocked: 18,
+  });
+
+  redirectTest({
+    method: 'description',
+    returnsWhenMocked: 'some random description',
+  });
+
+  redirectTest({
+    method: 'version',
+    returnsWhenMocked: BigNumber.from(2),
+  });
+
+  redirectTest({
+    method: 'latestRoundData',
+    returnsWhenMocked: [BigNumber.from(1), BigNumber.from(2), BigNumber.from(3), BigNumber.from(4), BigNumber.from(5)],
+  });
+
+  /**
+   * This test makes sure that when a method is called and the feed is not set, then the call reverts.
+   * However, when the method is called and there is a feed set, then the return value is just redirected
+   * from the feed, to the registry's caller
+   */
+  function redirectTest<Key extends keyof Functions>({
+    method,
+    returnsWhenMocked: returnValue,
+  }: {
+    method: Key;
+    returnsWhenMocked: Awaited<ReturnType<Functions[Key]>>;
+  }) {
+    describe(method, () => {
+      when('feed proxy is not set', () => {
+        then(`calling ${method} will revert with message`, async () => {
+          await behaviours.txShouldRevertWithMessage({
+            contract: registry,
+            func: method,
+            args: [constants.NOT_ZERO_ADDRESS, USD],
+            message: 'FeedNotFound',
+          });
+        });
+      });
+      when('feed registry is set', () => {
+        given(async () => {
+          await registry.connect(governor).setFeedProxy(LINK, USD, feed.address);
+          feed[method].returns(returnValue);
+        });
+        then('return value from feed proxy is returned through registry', async () => {
+          const result = await registry[method](LINK, USD);
+          expect(result).to.eql(returnValue);
+        });
+      });
+    });
+  }
+  type Keys = keyof AggregatorV3Interface['functions'] & keyof ChainlinkRegistry['functions'];
+  type Functions = Pick<AggregatorV3Interface & ChainlinkRegistry, Keys>;
+  type Awaited<T> = T extends PromiseLike<infer U> ? U : T;
+});
