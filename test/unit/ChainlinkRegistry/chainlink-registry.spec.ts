a7c7df0ef225a5e828f8936e44bf2ba2f39f37ab
Nicolas Chamo <nicolas@chamo.com.ar>
2022-03-15 16:39:15 -0300

    fix: fix typings in unit test

--- test/unit/ChainlinkRegistry/chainlink-registry.spec.ts
+++ test/unit/ChainlinkRegistry/chainlink-registry.spec.ts
@@ -133,12 +133,12 @@ contract('ChainlinkRegistry', () => {
    * However, when the method is called and there is a feed set, then the return value is just redirected
    * from the feed, to the registry's caller
    */
-  function redirectTest<Key extends keyof Functions>({
+  function redirectTest<Key extends keyof Functions, ReturnValue extends Awaited<ReturnType<Functions[Key]>>>({
     method,
     returnsWhenMocked: returnValue,
   }: {
     method: Key;
-    returnsWhenMocked: Awaited<ReturnType<Functions[Key]>>;
+    returnsWhenMocked: Arrayed<ReturnValue> | ReturnValue;
   }) {
     describe(method, () => {
       when('feed proxy is not set', () => {
@@ -164,6 +164,7 @@ contract('ChainlinkRegistry', () => {
     });
   }
   type Keys = keyof AggregatorV3Interface['functions'] & keyof ChainlinkRegistry['functions'];
-  type Functions = Pick<AggregatorV3Interface & ChainlinkRegistry, Keys>;
+  type Functions = Pick<AggregatorV3Interface['functions'] & ChainlinkRegistry['functions'], Keys>;
   type Awaited<T> = T extends PromiseLike<infer U> ? U : T;
+  type Arrayed<T> = T extends Array<infer U> ? U : T;
 });
