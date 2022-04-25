9cbc3923145e0f4c82753765ab343887efb04f42
Ged <0xged@protonmail.com>
2022-04-25 20:29:20 -0300

    fix: ts-path on exported file

--- deploy/001_registry.ts
+++ deploy/001_registry.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { networkBeingForked } from '@test-utils/evm';
-import { IFeedRegistry__factory } from '@typechained';
+import { IFeedRegistry__factory } from '../typechained';
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer, governor } = await hre.getNamedAccounts();
